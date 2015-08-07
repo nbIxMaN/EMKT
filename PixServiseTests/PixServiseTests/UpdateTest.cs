ca881873f6624516a0e8c2bdcc73cadea3a50ff0
Anastasia <an.klyonskaya@gmail.com>
2015-08-07 13:46:36 +0300

    m

---
+++ PixServiseTests/PixServiseTests/UpdateTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using System.Reflection;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+using PixServiseTests.PixServise;
+using PixServiseTests.EMKServise;
+
+
+namespace PixServiseTests
+{
+    [TestFixture]
+    public class UpdateTest: Data
+    {
+        [Test]
+        public void test()
+        {
+
+        }
+    }
+}
