ff8a73cea21b84cf58049973860ad6ae7de2817d
Anastasia <an.klyonskaya@gmail.com>
2015-08-10 12:49:21 +0300

    new class

---
+++ PixServiseTests/PixServiseTests/Methods Tests/AddMedRecordTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using System.Reflection;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+using PixServiseTests.PixServise;
+using PixServiseTests.EMKServise;
+
+namespace PixServiseTests.Methods_Tests
+{
+    [TestFixture]
+    class AddMedRecordTest
+    {
+
+        [TearDown]
+        public void Clear()
+        {
+            Global.errors3.Clear();
+            Global.errors2.Clear();
+            Global.errors1.Clear();
+        }
+    }
+}
---
+++ PixServiseTests/PixServiseTests/Methods Tests/CloseCaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using System.Reflection;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+using PixServiseTests.PixServise;
+using PixServiseTests.EMKServise;
+
+namespace PixServiseTests.Methods_Tests
+{
+    [TestFixture]
+    public class CloseCaseTest:Data
+    {
+        [TearDown]
+        public void Clear()
+        {
+            Global.errors3.Clear();
+            Global.errors2.Clear();
+            Global.errors1.Clear();
+        }
+    }
+}
